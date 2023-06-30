4c8bc4eb8f042bda1633d0b046dec151fbd58a50
junioragepe19 <arnaldodossantosjunior022@gmail.com>
2023-06-29 21:05:59 -0300

    feat: Adicionado efeito ao passar mouse nos cards

--- LandingPage/src/views/Home.vue
+++ LandingPage/src/views/Home.vue
@@ -1,3 +1,27 @@
+<script setup>
+import BrowseGames from '@/components/BrowseGames.vue';
+import Ashe from '../assets/Character.png';
+import Pubg from '../assets/Pubg.png';
+import Val from '../assets/Omen.png';
+import Counter from '../assets/CS.png';
+import Header from '@/components/Header.vue';
+import Populars from '@/components/Populars.vue';
+import Hero from '@/components/Hero.vue';
+import BecomeOrganizator from '@/components/BecomeOrganizator.vue';
+import { computed, ref } from 'vue';
+
+const isHoving = ref(false);
+
+function changeHover(){
+  console.log(isHoving.value)
+  isHoving.value = !isHoving.value;
+  console.log(isHoving.value)
+}
+
+const valueHover = computed( () => {
+  return isHoving.value; })
+</script>
+
 <template>
   <Header class="header" />
   <Hero />
@@ -6,12 +30,12 @@
     <p class="view">View All</p>
   </div>
   <div class="d-flex d-flex-column populars">
-    <Populars :url="Ashe" entry="Free" color="background-color: #BEF970" colorText="color: #000" mode="1V1" prize="650" title="Path\nto Power" subtitle='League Of Legends' />
-    <Populars :url="Pubg" entry="Free" color="background-color: #E6F0D8" colorText="color: #000" mode="2V2" prize="50" title="Dutiful\nDeathMatch" subtitle='Playerunknown`s\nBattlegrounds ' />
-    <Populars :url="Val" entry="Free" color="background-color: #46800A" colorText="color: #fff" mode="2V2" prize="650" title="Open fire all\nStars" subtitle='Valorant' />
-    <Populars :url="Counter" entry="Free" color="background-color: #E6F0D8" colorText="color: #000" mode="5V5" prize="650" title="Cyl\nCSGO" subtitle='Counter Strike\nGlobal Offensive' />
+    <Populars class="popular" @mouseenter.once="changeHover" @mouseleave="changeHover" :class="valueHover ? 'nothover' : ''" :url="Ashe" entry="Free" color="background-color: #BEF970" colorText="color: #000" mode="1V1" prize="650" title="Path\nto Power" subtitle='League Of Legends' />
+    <Populars class="popular" :url="Pubg" entry="Free" @mouseenter="changeHover" @mouseleave="changeHover" :class="valueHover ? 'nothover' : ''"  color="background-color: #E6F0D8" colorText="color: #000" mode="2V2" prize="50" title="Dutiful\nDeathMatch" subtitle='Playerunknown`s\nBattlegrounds ' />
+    <Populars class="popular" :url="Val" entry="Free" @mouseenter="changeHover" @mouseleave="changeHover" :class="valueHover ? 'nothover' : ''" color="background-color: #46800A" colorText="color: #fff" mode="2V2" prize="650" title="Open fire all\nStars" subtitle='Valorant' />
+    <Populars class="popular" :url="Counter" entry="Free" @mouseenter="changeHover" @mouseleave="changeHover" :class="valueHover ? 'nothover' : ''" color="background-color: #E6F0D8" colorText="color: #000" mode="5V5" prize="650" title="Cyl\nCSGO" subtitle='Counter Strike\nGlobal Offensive' />
   </div>
-  <BrowseGames class="browseGames" />
+  <BrowseGames class="browseGames"/>
   <BecomeOrganizator />
 </template>
 
@@ -25,6 +49,17 @@
     text-align: right;
     color: white;
     border-bottom: 2px solid #BEF970;
+  }
+  .popular{
+    transition: 0.2s ease-in-out;
+  }
+  .popular:hover{
+    transform: scale(1.02);
+    z-index: 11;
+    filter: grayscale(0) !important;
+  }
+  .nothover{
+    filter: grayscale(1);
   }
   .titlePopular{
     justify-content: space-between;
@@ -42,15 +77,5 @@
     margin-bottom: 5rem;
   }
 </style>
-<script setup>
-import BrowseGames from '@/components/BrowseGames.vue';
-import Ashe from '../assets/Character.png';
-import Pubg from '../assets/Pubg.png';
-import Val from '../assets/Omen.png';
-import Counter from '../assets/CS.png';
-import Header from '@/components/Header.vue';
-import Populars from '@/components/Populars.vue';
-import Hero from '@/components/Hero.vue';
-import BecomeOrganizator from '@/components/BecomeOrganizator.vue';
-</script>
+
 
