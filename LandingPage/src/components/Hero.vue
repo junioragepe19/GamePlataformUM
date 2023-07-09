ac98a6f4ef980676432c68e8d95ef1c442808998
junioragepe19 <arnaldodossantosjunior022@gmail.com>
2023-07-08 22:02:07 -0300

    fix: atualizando data

--- LandingPage/src/components/Hero.vue
+++ LandingPage/src/components/Hero.vue
@@ -1,13 +1,16 @@
 <script setup>
     import CustomButton from './CustomButton.vue';
     import { ref, watch } from 'vue';
-    var countDownDate = new Date("July 05, 2023 17:00:00").getTime();
+    import { useDisplay } from 'vuetify/lib/framework.mjs';
+    import InteractiveButton from './InteractiveButton.vue';
+    var countDownDate = new Date("July 15, 2023 17:00:00").getTime();
     var hours = ref(10);
     var minutes = ref(24);
     var seconds = ref(60);
     var isVideoPlaying = ref(false);
     const formattedTime = ref('12');
     var isfinished = ref(true)
+    const {width, mobile} = useDisplay()
     var interval = setInterval(function(){
         var countDownToday = Date.now();
         var difference = countDownDate - countDownToday;
@@ -43,60 +46,87 @@
 </script>
 
 <template>
-    <div class="background d-flex flex-column">
-        <h1 class="bgtitle d-flex">
-            All the kings are fighting
-        </h1>
-        <p class="subtitleHero">Hurry to join in the greatest Valorant tournament of all time. Fight till the end and get the reward</p>
-            <div class="blur"></div>  
-            <div class="character">
-                  
-                <img class="img" src="../assets/Phoenix.png" />
-            </div>
-        
-        <div class="secondSection">
+    <div class="background d-flex" :style="width < 1100 ? 'flex-direction: column' : ''" >
+        <div class="firstSection d-flex flex-column">
+            <h1 class="bgtitle d-flex">
+                All the kings are fighting
+            </h1>
+            <p class="subtitleHero">Hurry to join in the greatest Valorant tournament of all time. Fight till the end and get the reward</p>
+                <div class="blur"></div> 
+                <div class="character">
+                    
+                    <img class="img" src="../assets/Phoenix.png" />
+                </div>
+        </div>
+        <div class="d-flex secondSection">
+            <div class="contentSection">
             <div class="polygon">
             </div>
             <div class="content_video">
-                <div class="formatRight"></div>
-                <div class="formatBottom2"></div>
-                <div class="formatLeft"></div>
-                <div class="formatBottom"></div>
                 <div class="format d-flex">
-                    <p class="date">July</p>
-                    <p class="dateNumber">05</p>
-                    <p class="dateFull">{{ isfinished ? formattedTime : "Expired" }}</p>
-                </div>
-                <div class="format2 d-flex">
-                    <div v-if="!isVideoPlaying" class="play d-flex">
-                        <div class="componentPlay d-flex" @click="playVideo">
-                            <v-icon color="#D2E041" size="large">mdi-play</v-icon>
-                        </div>
+                    <div class="d-flex dateTournament">
+                        <p class="date">July</p>
+                        <p class="dateNumber">15</p>
+                        <p class="dateFull">{{ isfinished ? formattedTime : "Expired" }}</p>
                     </div>
-                    <video class="video" id="video"
-                    @pause="stopVideo"
-                    @loadstart="this.volume=0.1"
-                    poster="https://i0.wp.com/thegamerstation.com/wp-content/uploads/2023/03/Valorant-oynarken-nasil-cevrimdisi-offline-gorunebilirim.png?fit=1200%2C675&ssl=1" width="180" height="140">
-                        
-                        <source src="../assets/Trailer.mp4" type="video/mp4">
-                        
-                    </video>    
+                    <div class="format2 d-flex d-flex-column">
+                        <div v-if="!isVideoPlaying" class="play d-flex">
+                            <div class="componentPlay d-flex" @click="playVideo">
+                                <v-icon color="#D2E041" size="large">mdi-play</v-icon>
+                            </div>
+                        </div>
+                        <video class="video" id="video"
+                        @pause="stopVideo"
+                        @loadstart="this.volume=0.1"
+                        poster="https://i0.wp.com/thegamerstation.com/wp-content/uploads/2023/03/Valorant-oynarken-nasil-cevrimdisi-offline-gorunebilirim.png?fit=1200%2C675&ssl=1" width="180" height="140">
+                            
+                            <source src="../assets/Trailer.mp4" type="video/mp4">
+                        </video>   
+                    </div>
                 </div>
-                <custom-button class="button" />
-                <div class="nameTournament">
+                
+                <!-- <div class="join">
+                    <interactive-button text-button="Join Tournament" :is-icon='true' color-text="black" color="#99EE2D" icon="mdi-arrow-right-thick" />
+                </div>  -->
+                <!-- <div class="nameTournament">
                     <p class="bntitle">Valorant Tournament</p>
-                </div>
+                </div> -->
             </div>
+        </div>
         </div>
     </div>
 </template>
 
 <style>
 @import '../app.scss';
-.secondSection{
-    position: absolute;
-    top: 0;
+.firstSection{
+    position: relative;
     width: 100%;
+}
+.dateTournament{
+    display: flex;
+    flex-direction: column;
+    width: 35%;
+    align-items: center;
+}
+.contentSection{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: right;
+    position: absolute;
+    right: 0;
+    top: 0;
+}
+.join{
+    position: absolute;
+    width: 50%;
+    right: 0;
+    height: 15% !important;
+}
+.secondSection{
+    position: relative;
+    width: 60%;
     height: 100%;
 }
 .nameTournament{
@@ -123,6 +153,7 @@
     align-items: center;
     justify-content: center;
     height: 25%;
+    flex-wrap: wrap;
     width: 25%;
     background-color: rgba(0, 0, 0, 0.8);
     border-radius: 5px;
@@ -136,42 +167,8 @@
 }
 .video{
     margin: auto;
-}
-.formatRight{
-    position: absolute;
-    left: 23rem;
-    top: 10.3rem;
-    height: 3.75rem;
-    width: 5px;
-    background-color: rgb(255, 255, 255, 0.07);
-}
-.formatBottom2{
-    justify-content: center;
-    position: absolute;
-    left: 23rem;
-    top: 10rem;
-    right: 15rem;
-    height: 5px;
-    width: 12rem;
-    background-color: rgb(255, 255, 255, 0.07);
-}
-.formatBottom{
-    position: absolute;
-    left: 15.3rem;
-    top: 13.74rem;
-    right: 14rem;
-    height: 5px;
-    width: 7.7rem;
-    background-color: rgb(255, 255, 255, 0.07);
-}
-.formatLeft{
-    position: absolute;
-    left: 15rem;
-    top: 0.24rem;
-    right: 14rem;
-    height: 13.8rem;
-    width: 5px;
-    background-color: rgb(255, 255, 255, 0.07);
+    width: 100%;
+    height: 100%;
 }
 .dateFull{
     font-family: 'bntitle';
@@ -193,57 +190,35 @@
 }
 .content_video{
     position: relative;
-    right: -35rem;
-    top:15rem;
+    display: flex;
+    justify-content: right;
+    margin-right: 15%;
+    top: 10%;
+    width: 100%;
+    height: 100%;
 }
 .format2{
     position: absolute;
-    right: 15rem;
-    left: 23rem;
-    height: 10rem;
-    width: 12rem;
-    border-top-right-radius: 2px;
-    border-bottom-right-radius: 2px;
-    background-color: rgb(255, 255, 255, 0.05);
-}
-.format2::before {
-    content: "";
-    position: absolute;
-    top: 0px;
-    width: calc(100%);
-    height: 4px;
-    background-color: rgb(255, 255, 255, 0.07);
+    right: 1%;
+    top: 7%;
+    height: 50%;
+    width: 57%;
 }
 .format{
-    align-items: center;
-    flex-direction: column;
-    box-sizing: content-box;
+    clip-path: polygon(50% 0%, 100% 0, 100% 63%, 37% 62%, 37% 100%, 0 100%, 0 0);
     position: absolute;
-    border-top-left-radius: 2px;
-    border-bottom-right-radius: 2px;
-    border-bottom-left-radius: 2px;
-    right: 15rem;
-    left: 15rem;
-    height: 10rem;
-    width: 8rem;
-    border-bottom: rgb(255, 255, 255, 0) solid 4rem;
+    height: 45%;
+    width: 75%;
     background-color: rgb(255, 255, 255, 0.05);
-}
-.format::before {
-    content: "";
-    position: absolute;
-    top: 0px;
-    width: calc(100%);
-    height: 4px;
-    background-color: rgb(255, 255, 255, 0.07);
+    border: 0.3rem solid rgba(255, 255, 255, 0.3);
 }
 .polygon{
     transform: rotate(45deg);
     position: absolute;
     height: 10rem;
     width: 10rem;
-    right: 10rem;
-    top: 10rem;
+    top: 0;
+    right: 10%;
     flex-shrink: 0;
     background-color: #bef970;
     box-shadow: 1px 1px 96px 10px rgba(190,249,112,0.75);
@@ -252,17 +227,18 @@
 }
 .character{
     height: 100%;
-    width: 80%;
+    width: 100%;
 }
 .img{
     transform: rotate(8.15deg);
-    top: 13rem;
-    left: 27.8rem;
+    top: 7%;
+    left: 52%;
     position: absolute;
-    height: 80%;
+    height: 100%;
 }
 .subtitleHero{
     position: relative;
+    padding: 1%;
     width: 33%;
     height: 50%;
     color: #fff;
@@ -270,9 +246,8 @@
     font-size: 1.1rem;
 }
 .bgtitle{
-    position: relative;
-    width: 60%;
-    height: 75%;
+    width: 100%;
+    height: 50%;
     font-family: 'bntitle';
     color: white;
     font-size: 8rem;
